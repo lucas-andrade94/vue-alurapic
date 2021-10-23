b8b7523ec00190cabd796764e00ae3a466b2f96d
Lucas Andrade <andradelucas94@gmail.com>
2021-10-23 17:19:40 -0600

    Starting application

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="corpo">
+    <Menu :rotas="routes" />
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+import { routes } from "./routes";
+import Menu from "./components/shared/menu/Menu.vue";
+
+export default {
+  components: {
+    Menu
+  },
+
+  data() {
+    return {
+      routes
+    };
+  }
+};
+</script>
+
+<style>
+.corpo {
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+
+.pagina-enter-active,
+.pagina-leave-active {
+  transition: opacity 0.3s;
+}
+
+.pagina-enter,
+.pagina-leave-active {
+  opacity: 0;
+}
+</style>
---
+++ src/components/cadastro/Cadastro.vue
@@ -0,0 +1,15 @@
+<<template>
+  <div>
+    <h2 class="centralizado">Cadastro</h2>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  .centralizado {
+    text-align: center;
+  }
+</style>
